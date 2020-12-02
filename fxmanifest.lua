a47087d26d06a4e5dacd1baebf5831fcdc731ece
Lentokone <44865524+Aik-10@users.noreply.github.com>
2020-12-02 10:56:11 +0200

    mastered

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_script 'server/*.js'
+client_script 'client/*.js'
+
+ui_page 'nui/ui.html'
+
+files {
+    'nui/ui.html',
+    'nui/css/*.css',
+    'nui/js/*.js',
+    'nui/files/*.svg',
+}
